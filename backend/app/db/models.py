9a44090696938f04062a07feb8b2916db3671de5
ralphribeiro <ralph_ribeiro@msn.com>
2020-12-08 21:01:13 -0300

    create item and local models

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, Numeric, ForeignKey
+from sqlalchemy.orm import backref, relationship
 
 from .session import Base
 
@@ -14,3 +15,26 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     is_superuser = Column(Boolean, default=False)
+
+
+class Local(Base):
+    __tablename__ = "local"
+
+    id = Column(Integer, primary_key=True, index=True)
+    volume = Column(Integer)
+    coordinate = Column(String, nullable=False)
+
+    items = relationship("Item", back_populates="locale")
+
+
+class Item(Base):
+    __tablename__ = "item"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False, index=True)
+    description = Column(String)
+    volume = Column(Integer, nullable=False)
+    price = Column(Numeric)
+    local_id = Column(Integer, ForeignKey('local.id'))
+
+    locale = relationship("Local", back_populates="items")
