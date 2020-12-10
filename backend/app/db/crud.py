2dc5861b9d29e11a50767398086f6264c3517345
ralphribeiro <ralph_ribeiro@msn.com>
2020-12-10 15:54:35 -0300

    return added in create_item

--- backend/app/db/crud.py
+++ backend/app/db/crud.py
@@ -94,6 +94,7 @@ def create_item(db: Session, item: schemas.ItemCreate):
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
+    return db_item
 
 
 def delete_item(db: Session, item_id: int):
